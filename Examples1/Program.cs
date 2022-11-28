05b903d33163edebf081408db62bacd23bb62ef6
Mikeeer <my email>
2022-11-28 16:31:20 +0300

    Первое задание

---
+++ Examples1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите первое число:");
+int namber1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int namber2 = int.Parse(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (namber1 > namber2)
+    {
+        max = namber1;
+        min = namber2;   
+    }
+
+else 
+    {
+        max = namber2; 
+        min = namber1;
+    }    
+
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");

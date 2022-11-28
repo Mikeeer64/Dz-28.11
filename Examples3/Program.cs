e5d67c47cec21a140a56c29830ca5655cf665f7c
Mikeeer <my email>
2022-11-28 17:27:25 +0300

    Третье задание

---
+++ Examples3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число:");
+int namber = int.Parse(Console.ReadLine());
+
+if (namber % 2 == 0)
+    Console.WriteLine("Является чётным");
+else Console.WriteLine("Не является чётным");
+
+   
